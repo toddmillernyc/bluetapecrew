db4b6e27474f053995b3e90584c8f21283cac76a
Todd Miller <todd@millerwebdev.com>
2017-12-23 20:20:37 -0500

    added test email feature to admin

---
+++ BlueTapeCrew/Areas/Admin/Controllers/EmailTestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BlueTapeCrew.Contracts.Services;
+using BlueTapeCrew.Utils;
+
+namespace BlueTapeCrew.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class EmailTestController : ApiController
+    {
+        private readonly ISiteSettingsService _siteSettingsService;
+        private readonly IEmailService _emailService;
+
+        public EmailTestController(ISiteSettingsService siteSettingsService, IEmailService emailService)
+        {
+            _siteSettingsService = siteSettingsService;
+            _emailService = emailService;
+        }
+
+        public async Task<IHttpActionResult> Get(string email)
+        {
+            try
+            {
+                var request = EmailHelper.GetTestEmailRequest(await _siteSettingsService.Get(), email);
+                await _emailService.SendEmail(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ BlueTapeCrew/Utils/EmailHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using BlueTapeCrew.Models;
+
+namespace BlueTapeCrew.Utils
+{
+    public static class EmailHelper
+    {
+        public static SmtpRequest GetTestEmailRequest(SiteSetting settings, string email)
+        {
+            var order = GetTestOrder(email);
+            var textBody = EmailTemplates.GetOrderConfirmationTextBody(order, true);
+            var htmlBody = EmailTemplates.GetOrderConfirmationHtmlBody(order);
+            return new SmtpRequest(settings, htmlBody, textBody, order.Email);
+        }
+
+        private static Order GetTestOrder(string email)
+        {
+            return new Order
+            {
+                Id = 1,
+                Invoice = new Invoice("abc"),
+                Total = 100.00m,
+                State = "NY",
+                City = "Manhattan",
+                Email = email,
+                InvoiceId = 2,
+                Phone = "555-555-5555",
+                Shipping = 5.00m,
+                UserName = "customeruser",
+                Address = "123 easy st",
+                SubTotal = 100.00m,
+                DateCreated = DateTime.UtcNow,
+                FirstName = "customer",
+                IpAddress = "192.168.1.1",
+                LastName = "Smith",
+                OrderItems = new List<OrderItem>
+                    {
+                        new OrderItem
+                        {
+                            Description = "Product description",
+                            Price = 95.00m,
+                            Quantity = 1,
+                            SubTotal = 95.00m
+                        }
+                    },
+                SessionId = "sessionabc",
+                Zip = "10001"
+            };
+        }
+    }
+}
