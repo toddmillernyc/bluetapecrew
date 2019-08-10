84a66cb6906490e15ff00b3fceba5b5428d8b4ca
Todd Miller <todd@toddmiller.nyc>
2019-08-10 12:55:29 -0400

    removing end2end tets while troubleshooting build pipeline

--- BlueTapeCrew.EndToEndTests/Extensions/WebElementExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenQA.Selenium;
-
-namespace BlueTapeCrew.EndToEndTests.Extensions
-{
-    public static class WebElementExtensions
-    {
-        public static string Href(this IWebElement element) => element.GetAttribute("href");
-    }
-}
