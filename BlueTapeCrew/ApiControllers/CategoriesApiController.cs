1fefd46f3349ccd79927aee4b03bf790cdb5b025
Todd Miller <todd@toddmiller.nyc>
2020-01-20 02:38:24 -0500

    updated packages.  added categoriesapi controller

---
+++ BlueTapeCrew/ApiControllers/CategoriesApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace BlueTapeCrew.ApiControllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesApiController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesApiController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async  Task<IActionResult> Get()
+        {
+            var model = await _categoryService.GetAllWithProducts();
+            return Ok(model);
+        }
+    }
+}
