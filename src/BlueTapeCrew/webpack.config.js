4ad6427baa7a74b41008e12a023fc380c214433d
Todd Miller <todd@toddmiller.nyc>
2020-02-01 16:34:04 -0500

    added babel, update webpack build config

---
+++ src/BlueTapeCrew/webpack.config.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    entry: {
+        app: './wwwroot/js/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/wwwroot'
+    }
+};
