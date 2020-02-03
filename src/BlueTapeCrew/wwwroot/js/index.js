861e49daafb1b1c39da77941feae780c91f8df38
Todd Miller <todd@toddmiller.nyc>
2020-02-03 05:37:26 -0500

    added owl carousel to webpack

--- src/BlueTapeCrew/wwwroot/js/index.js
+++ src/BlueTapeCrew/wwwroot/js/index.js
@@ -1,5 +1,8 @@
 ﻿import 'bootstrap/dist/css/bootstrap.css';
 import 'fancybox/dist/css/jquery.fancybox.css';
+import 'owl-carousel/owl-carousel/owl.carousel.css';
 
 $ = window.$ = window.jQuery = require('jquery');
 require('fancybox')($);
+require('owl-carousel/owl-carousel/owl.carousel.js');
+
