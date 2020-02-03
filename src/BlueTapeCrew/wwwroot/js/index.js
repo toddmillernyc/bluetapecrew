4987b52afe6692f4955c21040fd5dea775bb9a09
Todd Miller <todd@toddmiller.nyc>
2020-02-03 05:54:09 -0500

    added jquery uniform to webpack config

--- src/BlueTapeCrew/wwwroot/js/index.js
+++ src/BlueTapeCrew/wwwroot/js/index.js
@@ -1,8 +1,9 @@
 ﻿import 'bootstrap/dist/css/bootstrap.css';
 import 'fancybox/dist/css/jquery.fancybox.css';
 import 'owl-carousel/owl-carousel/owl.carousel.css';
+import 'font-awesome/css/font-awesome.css';
+import 'jquery-uniform/themes/default/css/uniform.default.css';
 
 $ = window.$ = window.jQuery = require('jquery');
 require('fancybox')($);
 require('owl-carousel/owl-carousel/owl.carousel.js');
-
