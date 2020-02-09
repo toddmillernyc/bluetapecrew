82c2e5ccdb565f8301c8ca342e083726062bf7b0
Todd Miller <todd@toddmiller.nyc>
2020-02-08 20:43:27 -0500

    more webpack updates

--- src/BlueTapeCrew/ClientApp/src/index.js
+++ src/BlueTapeCrew/ClientApp/src/index.js
@@ -48,6 +48,27 @@ $(document).ready(function () {
     Layout.initImageZoom();
     layout.initTouchspin();
     layout.initUniform();
+
+    $(".owl-carousel-home").owlCarousel({
+        pagination: false,
+        navigation: true,
+        items: 5,
+        lazyLoad: true,
+        addClassActive: true,
+        itemsCustom: [
+            [0, 1],
+            [320, 1],
+            [480, 2],
+            [660, 2],
+            [700, 3],
+            [768, 3],
+            [992, 4],
+            [1024, 4],
+            [1200, 5],
+            [1400, 5],
+            [1600, 5]
+        ]
+    });
 });
 
 function Layout() {
