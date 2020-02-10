b528ad65861b633954ee677ac7353184b7618d4e
Todd Miller <todd@toddmiller.nyc>
2020-02-09 19:44:12 -0500

    added router to react

--- src/admin/src/App.js
+++ src/admin/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import HomePage from './components/HomePage';
+import CategoriesPage from './components/CategoriesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/categories">
+            <CategoriesPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/admin/src/components/CategoriesPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class CategoriesPage extends React.Component {
+
+    render() {
+        return (
+            <h1>Categories</h1>
+        );
+    }
+}
