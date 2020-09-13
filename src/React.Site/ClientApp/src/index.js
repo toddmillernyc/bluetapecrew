bbd4adb5dc8c6c6e75879d354690d0c64e2c11c9
Todd Miller <todd@toddmiller.nyc>
2020-09-12 22:33:43 -0400

    added webpack and react

---
+++ src/React.Site/ClientApp/src/index.js
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return <div>Hello React,Webpack 4 & Babel 7!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ src/React.Site/ClientApp/webpack.config.js
@@ -0,0 +1,34 @@
+﻿var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../wwwroot')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
