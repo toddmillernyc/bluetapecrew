9b3149a6dd9a467efba0aceec4535c2585142dbc
Todd Miller <todd@toddmiller.nyc>
2020-09-13 00:21:03 -0400

    added dbcontext, added api controller

---
+++ src/React.Site/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace React.Site.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly BtcEntities _db;
+
+        public ApiController(BtcEntities db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(_db.Categories);
+        }
+    }
+}
