5822a3c5da9fad9d65b182e6f18c1793a770eea6
Todd Miller <todd@toddmiller.nyc>
2020-09-17 22:40:48 -0400

    added database mirgations

---
+++ src/Entities/Migrations/20200918023955_Initial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
