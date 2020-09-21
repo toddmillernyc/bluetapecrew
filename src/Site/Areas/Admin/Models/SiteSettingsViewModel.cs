4931c3fad336baa2a55b076914e49f23eca76176
Todd Miller <todd@toddmiller.nyc>
2020-09-21 01:06:51 -0400

    added new tab for site profile in admin

---
+++ src/Site/Areas/Admin/Models/SiteSettingsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Services.Models;
+
+namespace Site.Areas.Admin.Models
+{
+    public class SiteSettingsViewModel
+    {
+        public SiteSetting SiteSettings { get; set; }
+        public SiteProfile SiteProfile { get; set; }
+    }
+}
