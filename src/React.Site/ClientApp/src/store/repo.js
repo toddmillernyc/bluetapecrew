cd8c4a474181a5e5577d3730686166d035c26a6d
Todd Miller <todd@toddmiller.nyc>
2020-10-08 23:26:58 -0400

    moved graphql query to function body

--- src/React.Site/ClientApp/src/store/repo.js
+++ src/React.Site/ClientApp/src/store/repo.js
@@ -5,26 +5,30 @@ import { ApolloClient,InMemoryCache, gql } from "@apollo/client";
   cache: new InMemoryCache()
 });
 
-const GET_CATEGORIES = gql`
-{
-  categories(where: { published: true }, order_by: { position: ASC }) {
-    name
-    productCategories{
-      product {
-        id
-        productName
+export const getCategories = () => client.query({
+  query: gql`
+    {
+      categories(where: { published: true }, order_by: { position: ASC }) {
+        name
+        productCategories{
+          product {
+            id
+            productName
+          }
+        }
       }
     }
-  }
-}
-`;
-export const getCategories = () => client.query({query:GET_CATEGORIES});
+  `
+});
 
-const LOGIN_MUTATION = gql`
-mutation login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
-  }
-}
-`;
-export const login = credentials => client.mutate({ variables: credentials, mutation: LOGIN_MUTATION });
+export const login = credentials => client.mutate({ 
+  variables: credentials,
+  mutation: gql`
+    mutation login($email: String!, $password: String!) {
+      login(email: $email, password: $password) {
+        token
+      }
+    }
+  `
+});
+
