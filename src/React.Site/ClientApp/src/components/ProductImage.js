5dae97acfa6c2866383a67e2bdd7c96d0e4bf5f9
Todd Miller <todd@toddmiller.nyc>
2020-10-10 01:20:10 -0400

    removed image data from store

--- src/React.Site/ClientApp/src/components/ProductImage.js
+++ src/React.Site/ClientApp/src/components/ProductImage.js
@@ -1,12 +1,14 @@
-import React from 'react';
-
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchImageAsync, selectImage } from '../store/imagesSlice';
+import React, { useState, useEffect } from 'react';
+import { getImage } from '../data/repo';
 
 const ProductImage = ({ imageId, className }) => {
-  const dispatch = useDispatch();
-  dispatch(fetchImageAsync(imageId));
-  const imageSrc = useSelector(selectImage)[imageId];
+  const [imageSrc, setImageSrc] = useState(0);
+
+  useEffect(() => {
+    getImage(imageId).then(result => {
+      setImageSrc(result.data.imageData.src);
+    });
+  });
   return <img className={className} src={imageSrc && `data:image/png;base64,${imageSrc}`} />;
 }
 
--- src/React.Site/ClientApp/src/store/index.js
+++ src/React.Site/ClientApp/src/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './loginSlice';
 import categoriesReducer from './categoriesSlice';
 import siteProfileSliceRecuder from './siteProfileSlice';
-import imagesSliceReducer from './imagesSlice';
 import productsSliceReducer from './productsSlice';
 
 export default configureStore({
@@ -10,7 +9,6 @@ export default configureStore({
     login: loginReducer,
     categories: categoriesReducer,
     siteProfile: siteProfileSliceRecuder,
-    images: imagesSliceReducer,
     products: productsSliceReducer
   }
 });
