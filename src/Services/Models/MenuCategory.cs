40d888cc9cd1e404c574f0d9a9de881c4e2e5977
Todd Miller <todd@toddmiller.nyc>
2021-02-07 02:55:28 -0500

    added id to menu api

--- src/Services/Models/MenuCategory.cs
+++ src/Services/Models/MenuCategory.cs
@@ -4,6 +4,7 @@ namespace Services.Models
 {
     public class MenuCategory
     {
+        public string Id => Name.Trim().ToLower().Replace(" ", "-");
         public string Name { get; set; }
         public bool IsPublished { get; set; }
         public Dictionary<string, string> Products { get; set; }
