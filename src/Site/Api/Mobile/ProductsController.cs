8f250da21bfcb8a5c0f6530c8c022e61724c49fe
Todd Miller <todd@toddmiller.nyc>
2021-02-10 22:57:41 -0500

    added mobile products controller

---
+++ src/Site/Api/Mobile/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using Services.Models;
+
+namespace Site.Api.Mobile
+{
+    [Route("api/mobile/products/{productId}")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<Product> Get(int productId)
+        {
+            var product = await _productService.FindIncludeAll(productId);
+            return product;
+        }
+    }
+}
